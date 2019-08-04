bae5fb9840fdbf5718db6351c8cb010b53cd5d07
Winston Weinert <winston@ml1.net>
2019-08-04 17:43:45 -0500

    rename manual to avoid weird documentation links

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "mumble-ping")
-(define version "1.0.1")
+(define version "1.0.2")
 (define deps '("base" "typed-racket-lib" "rackunit-typed" "bitsyntax"))
 (define build-deps '("scribble-lib" "racket-doc" "typed-racket-doc"))
-(define scribblings '(("manual.scrbl" () (net-library))))
+(define scribblings '(("scribblings/mumble-ping.scrbl" () (net-library))))
