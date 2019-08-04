de7a9e80ec3599bb69d85ac4af1cf437c1a21d12
Winston Weinert <winston@ml1.net>
2019-08-04 17:16:25 -0500

    Fixup info.rkt

--- info.rkt
+++ info.rkt
@@ -1,5 +1,6 @@
 #lang info
 (define collection "mumble-ping")
-(define version "1.0.0")
-(define deps '("base" "typed-racket-lib" "bitsyntax"))
-(define scribblings '(("manual.scrbl" ())))
+(define version "1.0.1")
+(define deps '("base" "typed-racket-lib" "rackunit-typed" "bitsyntax"))
+(define build-deps '("scribble-lib" "racket-doc" "typed-racket-doc"))
+(define scribblings '(("manual.scrbl" () (net-library))))
