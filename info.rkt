2d177c219f959636f2ab2100576bb3a3a1f2b04d
Winston Weinert <winston@ml1.net>
2019-11-19 18:31:30 -0600

    Add undeclared depedency

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "mumble-ping")
-(define version "1.1.2")
-(define deps '("base" "typed-racket-lib" "rackunit-typed" "bitsyntax"))
+(define version "1.1.3")
+(define deps '("base" "rackunit-lib" "typed-racket-lib" "rackunit-typed" "bitsyntax"))
 (define build-deps '("scribble-lib" "racket-doc" "typed-racket-doc"))
 (define scribblings '(("scribblings/mumble-ping.scrbl" () (net-library))))
 (define pkg-info "Ping mumble servers")
